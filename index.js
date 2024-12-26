656f6ac767d1991e02b128dfcc57a6cd46aeb6f7
philip-hargreaves <137929060+philip-hargreaves@users.noreply.github.com>
2024-12-26 15:54:42 +0000

    create playRound function

--- index.js
+++ index.js
@@ -20,6 +20,35 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Draw! Both chose ${humanChoice}!`)
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            humanScore += 1;
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        } else {
+            computerScore += 1;
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore += 1;
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        } else {
+            computerScore += 1;
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanScore += 1;
+            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
+        } else {
+            computerScore += 1;
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        }
+    }  
+}
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();

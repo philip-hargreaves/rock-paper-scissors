86a3973b62ac45c6854bfe57482072c74ecc4aef
philip-hargreaves <137929060+philip-hargreaves@users.noreply.github.com>
2025-01-02 20:24:32 +0000

    Refactor playGame function logic

--- index.js
+++ index.js
@@ -17,33 +17,19 @@ function getHumanChoice() {
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        drawCount += 1
-        console.log(`Draw! Both chose ${humanChoice}!`)
-    } else if (humanChoice === "rock") {
-        if (computerChoice === "scissors") {
-            humanScore += 1;
-            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-        } else {
-            computerScore += 1;
-            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
-        }
-    } else if (humanChoice === "paper") {
-        if (computerChoice === "rock") {
-            humanScore += 1;
-            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-        } else {
-            computerScore += 1;
-            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
-        }
-    } else if (humanChoice === "scissors") {
-        if (computerChoice === "paper") {
-            humanScore += 1;
-            console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-        } else {
-            computerScore += 1;
-            console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
-        }
-    }  
+        drawCount += 1;
+        console.log(`Draw! Both chose ${humanChoice}!`);
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper") 
+    ) {
+        humanScore += 1;
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore += 1;
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    }
 }
 
 let humanScore = 0;
@@ -67,5 +53,6 @@ function playGame() {
 
     console.log(`Final Scores:\nPlayer: ${humanScore}, Computer: ${computerScore}, Draw: ${drawCount}`)
 }
+
 
 playGame()
